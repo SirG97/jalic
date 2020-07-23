3c0615dc78332da66de66874a871ef93330f50bf
SirG97 <sirgittarus@gmail.com>
2020-07-24 00:13:48 +0100

    fixing database issues

--- app/config/_env.php
+++ app/config/_env.php
@@ -3,6 +3,6 @@
 define('BASE_PATH', realpath(__DIR__.'/../../'));
 
 require_once __DIR__.'/../../vendor/autoload.php';
-
-$dotEnv =  Dotenv\Dotenv::createImmutable(BASE_PATH);
-$dotEnv->load();
+//
+//$dotEnv =  Dotenv\Dotenv::createImmutable(BASE_PATH);
+//$dotEnv->load();
