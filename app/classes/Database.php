725c707efe0a659535ae4306fa4c9303bf12dd15
SirG97 <sirgittarus@gmail.com>
2020-07-31 12:15:52 +0100

    fix the select input in the customer registration form

--- app/classes/Database.php
+++ app/classes/Database.php
@@ -11,16 +11,16 @@ class Database{
         $db = new Capsule();
 
         $db->addConnection([
-            'driver' => getenv('DB_DRIVER'),
-            'host' => getenv('DB_HOST'),
-            'database' => getenv('DB_NAME'),
-            'username' => getenv('DB_USERNAME'),
-            'password' => getenv('DB_PASSWORD'),
-//            'driver' => 'mysql',
-//            'host' => 'db4free.net',
-//            'database' => 'sarge_jalic',
-//            'username' => 'sarge_jalic',
-//            'password' => 'rrwcscrz1_jalic',
+//            'driver' => getenv('DB_DRIVER'),
+//            'host' => getenv('DB_HOST'),
+//            'database' => getenv('DB_NAME'),
+//            'username' => getenv('DB_USERNAME'),
+//            'password' => getenv('DB_PASSWORD'),
+            'driver' => 'mysql',
+            'host' => 'db4free.net',
+            'database' => 'sarge_jalic',
+            'username' => 'sarge_jalic',
+            'password' => 'rrwcscrz1_jalic',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => ''
